70510424ccfa5219fa6e80ed35c2b203b1085164
Nielsen Ramon <nielsenramon1@gmail.com>
2016-02-07 15:47:45 +0100

    Implement header

--- _assets/javascripts/application.js
+++ _assets/javascripts/application.js
@@ -1,1 +1,30 @@
 //= require_self
+
+// Smooth scrolling.
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+// Fade in animation
+
+window.sr = ScrollReveal({
+  delay: 200,
+  duration: 1000,
+  distance: '5px',
+  easing: 'ease-out',
+  scale: 1,
+  origin: 'top'
+});
+sr.reveal('.fade-in');
--- _assets/javascripts/vendor.js
+++ _assets/javascripts/vendor.js
@@ -1,1 +1,2 @@
 //= require jquery/dist/jquery
+//= require scrollreveal
