d8238e936c6b63b2de260cd9f79d77255f7d6a8e
Nielsen Ramon <nielsenramon1@gmail.com>
2016-05-10 22:12:29 +0200

    Add house page

--- _assets/javascripts/application.js
+++ _assets/javascripts/application.js
@@ -1,22 +1,5 @@
 //= require_self
 
-// Smooth scrolling.
-
-$(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
-});
-
 // Fixed navigation
 
 $(window).on('scroll', function() {
